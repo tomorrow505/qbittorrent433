cc5b6ee1c38260238d5f15ce4b530e412bd8d704
tomorrow505 <32202634+tomorrow505@users.noreply.github.com>
2021-03-10 11:00:10 +0800

    Update get_mediaifno_picture.sh

--- get_mediaifno_picture.sh
+++ get_mediaifno_picture.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # get get_mediaifno_picture.py and install necessary package(s).
-
-wget https://raw.githubusercontent.com/tomorrow505/qbittorrent433/main/get_mediaifno_picture.py
+cd $HOME
+wget https://raw.githubusercontent.com/tomorrow505/qbittorrent433/main/get_mediaifno_picture.py -O get_mediaifno_picture.py
 scritp_path=$(pwd)
 mkdir tmp && chmod 777 tmp
 

8dbef59763c1ba91899666530ff888aad2892d9e
tomorrow505 <32202634+tomorrow505@users.noreply.github.com>
2021-03-10 11:05:43 +0800

    Update get_mediaifno_picture.sh

--- get_mediaifno_picture.sh
+++ get_mediaifno_picture.sh
@@ -19,11 +19,12 @@ fi
 pip3 install pymediainfo pyimgbox qbittorrent-api
  
 # 创建一个传图命令
-touch /usr/bin/chuantu
-cd /usr/bin && chmod +x chuantu
-
-chuantu_path="/usr/bin/chuantu"
-cat>$chuantu_path<<EOF
-#!/bin/bash
-python3 "${script_path}get_mediaifno_picture.py" -i \$1
-EOF
+if ! [ -f /usr/bin/chuantu ]; then
+  touch /usr/bin/chuantu
+  cd /usr/bin && chmod +x chuantu
+  chuantu_path="/usr/bin/chuantu"
+  cat>$chuantu_path<<EOF
+  #!/bin/bash
+  python3 "${script_path}get_mediaifno_picture.py" -i \$1
+  EOF
+fi

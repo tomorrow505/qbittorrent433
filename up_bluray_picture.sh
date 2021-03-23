3c0425cc5694d06292d0678203bd8a62a8ef4bb6
tomorrow505 <32202634+tomorrow505@users.noreply.github.com>
2021-03-23 19:54:15 +0800

    Update up_bluray_picture.sh

--- up_bluray_picture.sh
+++ up_bluray_picture.sh
@@ -11,10 +11,13 @@ T
 EOF
 
 # 使用固定交互命令执行bluray并输出到output.data
+echo "正在截图……"
 bluray < input.data > output.data
 
+echo "读取截图路径……"
 # 读取图片存储路径
 path=$(cat output.data | sed -n '/Files are stored/p' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | cut -d'"' -f2)
 
+echo "开始上传截图……"
 # 调用python脚本搜索目录下的文件进行上传
 python3 /home/up_bluray_picture.py -p $path

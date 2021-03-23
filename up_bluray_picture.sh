bdd3a89a321594d449cb52f912eeb6f5c474ae8a
tomorrow505 <32202634+tomorrow505@users.noreply.github.com>
2021-03-23 19:40:45 +0800

    Update up_bluray_picture.sh

--- up_bluray_picture.sh
+++ up_bluray_picture.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 读取输入的路径写入固定交互
 cat>input.data<<EOF
 $1
 03
@@ -9,8 +10,11 @@ T
 \n
 EOF
 
+# 使用固定交互命令执行bluray并输出到output.data
 bluray < input.data > output.data
 
+# 读取图片存储路径
 path=$(cat output.data | sed -n '/Files are stored/p' | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | cut -d'"' -f2)
 
-python3 /home/send_picture_from_dir.py -p $path
+# 调用python脚本搜索目录下的文件进行上传
+python3 /home/up_bluray_picture.py -p $path
